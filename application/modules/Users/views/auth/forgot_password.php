9240951698af9dd26e1434a0450caddf321052cd
manis6062 <manis6062@gmail.com>
2017-04-19 12:18:57 +0545

    forgot password and reset password page

--- application/modules/Users/views/auth/forgot_password.php
+++ application/modules/Users/views/auth/forgot_password.php
@@ -1,15 +1,48 @@
-<h1><?php echo lang('forgot_password_heading');?></h1>
-<p><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+      <!DOCTYPE html>
+<html lang="en">
+    
+<head>
+        <title><?php echo $title ;?></title><meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="<?php echo PUBLIC_SITE_URL ; ?>css/bootstrap.min.css" />
+		<link rel="stylesheet" href="<?php echo PUBLIC_SITE_URL ; ?>css/bootstrap-responsive.min.css" />
+        <link rel="stylesheet" href="<?php echo PUBLIC_SITE_URL ; ?>css/matrix-login.css" />
+        <link href="<?php echo PUBLIC_SITE_URL ; ?>font-awesome/css/font-awesome.css" rel="stylesheet" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 
-<?php echo form_open("Users/auth/forgot_password");?>
+    </head>
+    <body>
+        <div id="loginbox">  
+            
+<!--            Display Error Message-->
+            <div id="infoMessage"><?php echo $message;?></div>
 
-      <p>
-      	<label for="identity"><?php echo (($type=='email') ? sprintf(lang('forgot_password_email_label'), $identity_label) : sprintf(lang('forgot_password_identity_label'), $identity_label));?></label> <br />
-      	<?php echo form_input($identity);?>
-      </p>
+                                 
+                <?php 
+                $attribute = array('class' => 'form-vertical');
+                echo form_open("Users/auth/forgot_password" , $attribute);?>
+				<p class="normal_text"><?php echo sprintf(lang('forgot_password_subheading'), $identity_label);?></p>
+				
+                    <div class="controls">
+                        <div class="main_input_box">
+                            <span class="add-on bg_lo"><i class="icon-envelope"></i></span>
+                            <?php 
+                                $identity['placeholder']='E-mail address';
+                                echo form_input($identity);?>
+                        </div>
+                    </div>
+               
+                <div class="form-actions">
+                    <p class="pull-right"><?php echo form_submit('submit', lang('forgot_password_submit_btn'),"class='btn btn-danger'");?></p>
+                </div>
+             <?php echo form_close();?> 
+        </div>
+        
+      <script src="<?php echo PUBLIC_SITE_URL ; ?>js/jquery-3.2.1.min.js"></script>   
+        <script src="<?php echo PUBLIC_SITE_URL ; ?>js/matrix.login.js"></script> 
+    </body>
 
-      <p><?php echo form_submit('submit', lang('forgot_password_submit_btn'));?></p>
+</html>
 
-<?php echo form_close();?>
+
